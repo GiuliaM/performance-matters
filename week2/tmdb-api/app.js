9efbb11c2bb34a91c50e3921a62107627c592e07
GiuliaM <Gekke_giulia@hotmail.com>
2017-03-22 15:52:52 +0100

    Working API call

--- week2/tmdb-api/app.js
+++ week2/tmdb-api/app.js
@@ -1,50 +1,42 @@
 /*   Modules    */
+//takes the environment variables from the .env and loads it into process.env
+//https://www.npmjs.com/package/dotenv
+require('dotenv').config()
 //gets the packages installed through npm
 var https = require('https')
 var express = require('express')
 var app = express()
 var concat = require('concat-stream') //adds all the data from a stream and calls callback with the result https://www.npmjs.com/package/concat-stream
-
+var request = require('request')
 //linking to the .env file to get the hidden key and url
 var url = process.env.searchApi;
+var posterUrl = 'http://image.tmdb.org/t/p/w500'
 var apiKey = process.env.apiKey;
-
-//takes the environment variables from the .env and loads it into process.env
-//https://www.npmjs.com/package/dotenv
-require('dotenv').config()
 
 app.use(express.static('static'))
 
-//movieRouter = require('./routes/movies');
 
 /*   View    */
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 /*   index    */
-app.get('/movies', function (req, res) {
+app.get('/', function (req, res) {
+    console.log(url);
     request(url, function(error, response, body){
-            var data = JSON.parse(body)
-            res.render('index.ejs', {movies: data})
+            console.log(error);
+            console.log(response);
+
+            var data = JSON.parse(body);
+
+            res.render('index.ejs', {
+                        movies: data.results,
+                        url: posterUrl
+                     })
         });
-})
-
-//app.get('/search/:query?', function (req, res) {
-//    var query = req.params.query;
-//
-//    function callback{
-        res.render('index')
-//    }
-})
+});
 
 
-//var data = require("./results.js")
-//console.log(data)
-/*   Npm server    */
-//app.listen(3000, function () {
-//  console.log('Example app listening on port 3000!')
-//})
-
-var server = app.listen(3000, function(){
-    console.log('server is running on port 3004')
+app.listen(3000, function(){
+    console.log('server is running on port 3000')
 })
