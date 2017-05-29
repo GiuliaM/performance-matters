c50b0d74a1d74aab772dc69012a62d2b56bdb12a
GiuliaM <Gekke_giulia@hotmail.com>
2017-05-29 17:54:27 +0200

    adjustments to sw

--- week2/tmdb-api/sw.js
+++ week2/tmdb-api/sw.js
@@ -1,7 +1,7 @@
 var cacheName = 'v2';
 var cacheFiles = [ //files that need to be cached
-    '/static/bundle.js', // evt . toevoegen
-    '/static/style.css', // evt . toevoegen
+    '/static/bundle.js',
+    '/static/style.css',
     'https://fonts.googleapis.com/css?family=Alegreya+Sans:300,400,700'
 ]
 
@@ -36,31 +36,24 @@ self.addEventListener('activate', function(event) {
     )
 })
 
-self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
+self.addEventListener('fetch', function(event) {
+    console.log('[ServiceWorker] Fetch', event.request.url);
 
     // e.respondWidth Responds to the fetch event
-    e.respondWith(
-
-        // Check in cache for the request being made
-        caches.match(e.request)
-
-
-            .then(function(response) {
-
+    event.respondWith(
+       //does the requested url exist, then start function(response)
+        caches.match(event.request).then(function(response) {
                 // If the request is in the cache
-                if ( response ) {
-                    console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+                if (response) {
+                    console.log("[ServiceWorker] Found in Cache", event.request.url, response);
                     // Return the cached version
                     return response;
                 }
 
                 // If the request is NOT in the cache, fetch and cache
-
-                var requestClone = e.request.clone();
+                var requestClone = event.request.clone(); //Need to clone request to save API images in cache
                 fetch(requestClone)
                     .then(function(response) {
-
                         if ( !response ) {
                             console.log("[ServiceWorker] No response from fetch ")
                             return response;
@@ -72,8 +65,8 @@ self.addEventListener('fetch', function(e) {
                         caches.open(cacheName).then(function(cache) {
 
                             // Put the fetched response in the cache
-                            cache.put(e.request, responseClone);
-                            console.log('[ServiceWorker] New Data Cached', e.request.url);
+                            cache.put(event.request, responseClone);
+                            console.log('[ServiceWorker] New Data Cached', event.request.url);
 
                             // Return the response
                             return response;
@@ -85,40 +78,6 @@ self.addEventListener('fetch', function(e) {
                         console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
                     });
 
-
             }) // end caches.match(e.request)
     ); // end e.respondWith
 });
-
-//self.addEventListener('fetch', function(event){
-//    console.log("[SW] Fetching", event.request.url);
-//    //responding to fetch event
-//    event.respondWith(
-//        //does the requested url exist, then start function(response)
-//        caches.match(event.request).then(function(response) {
-//            if (response) {
-//                console.log("[SW] found in cache", event.request.url);
-//                return response; //return cached version
-//            }
-//
-//            //Need to clone request to save API images in cache
-//            var requestClone = event.request.clone();
-//
-//            //if its not found in the cache, fetch it
-//            fetch(requestClone)
-//                .then(function(response) {
-//                    if (!response) {
-//                        console.log("[SW] No response from fetch");
-//                        return response;
-//                    }
-//
-//                var responseClone = response.clone();
-//
-//                caches.open(cacheName).then(function(cache) {
-//                    cache.put(event.request, responsClone);
-//                    return response;
-//                });
-//                })
-//        })
-//    )
-//});
